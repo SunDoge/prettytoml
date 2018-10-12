09210d808548a4b104c28f4de22ac50ad2d037fe
SunDoge <384813529@qq.com>
2018-10-12 17:56:59 +0800

    from file

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ mod tokens;
 
 use self::lexer::tokenize;
 use self::parser::parse_tokens;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 use std::path::Path;
 
 pub fn prettify(toml_text: &str) -> String {
@@ -20,6 +23,9 @@ pub fn prettify(toml_text: &str) -> String {
     "".to_string()
 }
 
-pub fn prettify_from_file<P: AsRef<Path>>(file_path: P) -> String {
-    "".to_string()
+pub fn prettify_from_file<P: AsRef<Path>>(file_path: P) -> Result<String, io::Error> {
+    let mut f = File::open(file_path)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    Ok(prettify(&contents))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate prettytoml;
 
-use prettytoml::prettify;
+use prettytoml::{prettify, prettify_from_file};
 
 fn main() {
     println!("Hello, world!");
+    let _ = prettify_from_file("Cargo.toml").expect("file not found");
+
     let formatted = prettify(
         r#"[database]
         server = "192.168.1.1"
